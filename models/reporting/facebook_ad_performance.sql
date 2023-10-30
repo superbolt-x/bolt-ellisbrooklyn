9cc9589043df4c2422772edbe43fe4823613c6f9
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-10-30 17:48:14 -0400

    Create facebook_ad_performance.sql

---
+++ models/reporting/facebook_ad_performance.sql
@@ -0,0 +1,33 @@
+{{ config (
+    alias = target.database + '_facebook_ad_performance'
+)}}
+
+SELECT 
+campaign_name,
+campaign_id,
+campaign_effective_status,
+campaign_type_default,
+CASE WHEN campaign_name ~* 'Ulta' THEN 'Ulta'
+    WHEN campaign_name ~* 'Sephora' THEN 'Sephora'
+    ELSE 'DTC'
+END as campaign_type_custom,
+adset_name,
+adset_id,
+adset_effective_status,
+audience,
+ad_name,
+ad_id,
+ad_effective_status,
+visual,
+copy,
+format_visual,
+visual_copy,
+date,
+date_granularity,
+spend,
+impressions,
+link_clicks,
+add_to_cart,
+purchases,
+revenue
+FROM {{ ref('facebook_performance_by_ad') }}
