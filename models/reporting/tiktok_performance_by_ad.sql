f228d3ce144b05328adfdbaf9c93a46a1a5fe849
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-10-30 17:50:00 -0400

    Create tiktok_performance_by_ad.sql

---
+++ models/reporting/tiktok_performance_by_ad.sql
@@ -0,0 +1,28 @@
+{{ config (
+    alias = target.database + '_tiktok_performance_by_ad'
+)}}
+
+SELECT 
+campaign_name,
+campaign_id,
+campaign_status,
+campaign_type_default,
+adgroup_name,
+adgroup_id,
+adgroup_status,
+CASE WHEN adgroup_name ~* 'Ulta' THEN 'Ulta'
+    WHEN adgroup_name ~* 'Sephora' THEN 'Sephora'
+END as adgroup_type_custom,
+audience,
+ad_name,
+ad_id,
+ad_status,
+visual,
+date,
+date_granularity,
+cost as spend,
+impressions,
+clicks,
+complete_payment_events as purchases,
+complete_payment_value as revenue
+FROM {{ ref('tiktok_performance_by_ad') }}
