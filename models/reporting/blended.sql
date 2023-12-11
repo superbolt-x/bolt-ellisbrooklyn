c0c0fadad0c7aaa9f1891d6b17f85cd676bc6451
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-11 17:33:11 -0500

    Update blended.sql

--- models/reporting/blended.sql
+++ models/reporting/blended.sql
@@ -47,6 +47,14 @@
     
     UNION ALL
     
+    (SELECT 'Adroll' as channel, date, date_granularity, 
+        COALESCE(SUM(spend),0) AS spend, COALESCE(SUM(impressions),0) AS impressions, COALESCE(SUM(clicks),0) AS clicks, COALESCE(SUM(purchases),0) AS purchases, 
+        COALESCE(SUM(revenue),0) AS revenue
+    FROM {{ source('reporting', 'adroll_daily_performance') }}
+    GROUP BY channel, date, date_granularity)
+    
+    UNION ALL
+    
     (SELECT 'Google - PMax' as channel, date, date_granularity, 
         COALESCE(SUM(spend),0) AS spend, COALESCE(SUM(impressions),0) AS impressions, COALESCE(SUM(clicks),0) AS clicks, COALESCE(SUM(purchases),0) AS purchases, 
         COALESCE(SUM(revenue),0) AS revenue
