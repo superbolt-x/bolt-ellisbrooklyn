883108d8cd5c891e3fd5f1f67ed7c749341275c4
Superbolt-X <67989957+superbolt-x@users.noreply.github.com>
2023-12-11 14:41:17 -0500

    Create adroll_daily_performance.sql

---
+++ models/reporting/adroll_daily_performance.sql
@@ -0,0 +1,51 @@
+{{ config (
+    alias = target.database + '_adroll_daily_performance'
+)}}
+
+SELECT DATE_TRUNC('day',date) as date, 'day' as date_granularity,
+spend,
+impressions,
+clicks,
+purchases,
+revenue
+FROM {{ source('gsheet_raw','adroll_daily_performance') }} 
+
+UNION ALL
+
+SELECT DATE_TRUNC('week',date) as date, 'week' as date_granularity,
+spend,
+impressions,
+clicks,
+purchases,
+revenue
+FROM {{ source('gsheet_raw','adroll_daily_performance') }} 
+
+UNION ALL
+
+SELECT DATE_TRUNC('month',date) as date, 'month' as date_granularity,
+spend,
+impressions,
+clicks,
+purchases,
+revenue
+FROM {{ source('gsheet_raw','adroll_daily_performance') }} 
+
+UNION ALL
+
+SELECT DATE_TRUNC('quarter',date) as date, 'quarter' as date_granularity,
+spend,
+impressions,
+clicks,
+purchases,
+revenue
+FROM {{ source('gsheet_raw','adroll_daily_performance') }} 
+
+UNION ALL
+
+SELECT DATE_TRUNC('year',date) as date, 'year' as date_granularity,
+spend,
+impressions,
+clicks,
+purchases,
+revenue
+FROM {{ source('gsheet_raw','adroll_daily_performance') }} 
